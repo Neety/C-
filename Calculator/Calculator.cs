0b73dd595c2991707edb6ba5628308f0a83e4517
Puneet Talapaneni <talapanenipuneet@gmail.com>
2022-04-24 13:15:28 +1000

    Calculator add function

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -7,6 +7,14 @@ namespace CSharpCalculator
         static void Main(String[] args)
         {
             Console.WriteLine("Hello World");
+
+            int num1;
+            int num2;
+
+            num1 = Console.Read();
+            num2 = Console.Read();
+
+            System.Console.WriteLine(num1 + num2);
         }
     }
 }
